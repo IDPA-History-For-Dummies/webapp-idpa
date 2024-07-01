63f4031a83ca414e1c10d0f80c809d82998106d7
Luis Meister » <l.lscher.inf21>
2024-07-01 20:14:30 +0200

    add remote api url

--- src/compositions/api.ts
+++ src/compositions/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-	baseURL: 'https://localhost:7167/informatics-api/v1/informatics', // Prolly no die falsch addresse
+	baseURL: 'https://programmingfordummiesapi.azurewebsites.net/informatics-api/v1/informatics', // Prolly no die falsch addresse
 	headers: {
 		'Content-Type': 'application/json',
 	},
