86e81d03780753f4ca49058dc09c1485c774d2b1
Luis Meister » <l.lscher.inf21>
2024-09-08 19:09:34 +0200

    fix

--- src/compositions/axios.ts
+++ src/compositions/axios.ts
@@ -3,6 +3,9 @@ import axios, {AxiosInstance} from "axios";
 //create axios instance
 const axiosInstance: AxiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_BASE_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 });
 
 export function useAxios(): AxiosInstance {
